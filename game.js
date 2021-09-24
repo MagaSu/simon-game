cb9603b4c85880c501856546247a1b1743066c5a
OneMadness <magamag268@gmail.com>
2021-09-24 16:50:22 +0200

    Writing script for the game

---
+++ game.js
@@ -0,0 +1,22 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function randomNumber() {
+  return Math.random(Math.floor);
+}
+
+green.addEventListener("click", function () {
+  console.log(event.target);
+});
+
+red.addEventListener("click", function () {
+  console.log(event.target);
+});
+
+yellow.addEventListener("click", function () {
+  console.log(event.target);
+});
+
+blue.addEventListener("click", function () {
+  console.log(event.target);
+});
